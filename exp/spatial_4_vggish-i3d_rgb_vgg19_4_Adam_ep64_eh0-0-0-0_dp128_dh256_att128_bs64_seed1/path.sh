eac8c9e4e440bda1753a5472c59d57e9eb4ffa3f
Idan Schwartz <idanschwartz@gmail.com>
2019-04-27 14:48:59 +0300

    init

--- exp/spatial_4_vggish-i3d_rgb_vgg19_4_Adam_ep64_eh0-0-0-0_dp128_dh256_att128_bs64_seed1/path.sh
+++
@@ -1,8 +0,0 @@
-# put additiona paths here
-export PATH=$PATH
-
-# if you use virtualenv, activate it here
-#. $HOME/work/venv/pytorch0.4/bin/activate
-
-# put additional python paths here
-export PYTHONPATH=./utils/coco-caption:$PYTHONPATH
